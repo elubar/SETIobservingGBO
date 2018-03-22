391ccb1bacfa4a5573c6a3b298c3d90b94f68894
shbhuk <shbhuk@gmail.com>
2018-03-22 09:20:05 -0400

    Sample scripts

---
+++ D_Day/Sample_midpoint_scripts/12-16_kepler446b.py
@@ -0,0 +1,64 @@
+
+    
+from __future__ import print_function
+    
+# Sample script to be reproduced to observe transit midpoint of target.
+    
+import time
+    
+import subprocess
+    
+import os
+
+    
+    
+# Directory in which the target catalogue is there
+    
+catdir = ''
+    
+# Specify target catalogue file name
+    
+catname = ''
+    
+    
+# Directory to log file
+    
+logdir = ''
+    
+# Log file name 
+    
+logfname = 'GBT_20180325_lband_psu.log'
+    
+logfile = os.path.join(logdir,logfname)
+    
+    
+# Path to Target catalogue
+    
+target_cat = os.path.join(catdir,catname)
+    
+Catalog(target_cat)
+    
+obs_time = 60.0 * 5
+    
+    
+midpoint = '12-16' # Specify local time of midpoint transit
+    
+targetname = 'kepler446b' 
+    
+    
+start = time.ctime(time.time())
+    
+print("Beginning tracking of '%s' at %s." %(targetname,start))
+    
+Track(targetname,endOffset=None,scanDuration=obs_time)
+    
+end = time.ctime(time.time())
+    
+print ("'%s' observed. Midpoint was at %s. Tracking ended at %s" %(targetname,midpoint,end))
+    
+    
+with open(logfile, 'a') as fh:
+    
+    fh.write("Tracking for target %s completed at %s" %(targetname,end))
+    
+    
--- make_cat_file.py
+++ make_cat_file.py
@@ -67,27 +67,19 @@ for i in range(0,len(targets)):
 ingress = np.array(["'"+i.strftime("%Y/%m/%d %H:%M:%S") + " UTC'" for i in ingress])
 egress = np.array(["'"+i.strftime("%Y/%m/%d %H:%M:%S") + " UTC'" for i in egress])
 
-<<<<<<< HEAD
-midpoint = np.array(["'"+i.datetime.strftime("%Y/%m/%d %H:%M:%S") + " UTC'" for i in midpoint])
-#t=Table([pair,name,ra,dec,ingress,midpoint,egress,np.zeros(len(pair))],names=('NAME','RA','DEC','PAIR','UTC_INGRESS','UTC_MIDPOINT','UTC_EGRESS','OBS'))
-#t=Table([name,ra,dec,pair,ingress,midpoint,egress,np.zeros(len(pair))],names=('','','','','','','',''))
-#location = os.path.dirname(__file__)
-#t.write(os.path.join(location,'D_Day',"GBT_20180325_lband_psu.cat"), format='ascii.fixed_width') 
-=======
 midpoint1 = np.array(["'"+i.datetime.strftime("%Y/%m/%d %H:%M:%S") + " UTC'" for i in midpoint])
 
 #t=Table([pair,name,ra,dec,ingress,midpoint,egress,np.zeros(len(pair))],names=('NAME','RA','DEC','PAIR','UTC_INGRESS','UTC_MIDPOINT','UTC_EGRESS','OBS'))
 t=Table([name,ra,dec,pair,ingress,midpoint1,egress,np.zeros(len(pair))],names=('','','','','','','',''))
 location = os.path.dirname(__file__)
 t.write(os.path.join(location,'D_Day',"GBT_20180325_lband_psu.cat"), format='ascii.fixed_width') 
->>>>>>> e9f7b5d0c9e1fc90b94cbc2616271613362a2a63
+
 '''
 f = open(os.path.join(location,'D_Day',"GBT_20180325_lband_psu.cat"),'r+')
 #f.seek(0)
 f.write('coordmode=J2000 \nHEAD = NAME    RA    DEC    PAIR    UTC_INGRESS    UTC_MIDPOINT     UTC_EGRESS    OBS                                     ')
 f.close()
 '''
-#'2018/03/25 10:03:55 UTC' 
 
 '''
 coordmode=J2000 
